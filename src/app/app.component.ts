1c4d6950dc857a6028c2bf0cf9b949e5700de623
Daniel Fabián <daniel@conectaentradas.com>
2024-03-19 12:02:03 +0100

    Adapt code to Angular Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,49 +9,54 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Proyecto-Inicial-Frontend-Angular';
+
+  readonly title = 'Proyecto-Inicial-Frontend-Angular';
+
+  readonly API_ORIGIN = 'https://proyecto-inicial-backend-agk6kyxhfa-uc.a.run.app/'
+
+
+  async sendData(data: FormData): Promise<unknown> {
+    const endpoint = new URL('/api/send-data/', this.API_ORIGIN)
+
+    const stringifiedData = JSON.stringify(data)
+
+    const response = await fetch(endpoint, {
+      method: 'POST',
+      body: stringifiedData,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+
+    return await response.json()
+  }
+
+  onFormSubmit(event: SubmitEvent) {
+    event.preventDefault()
+
+    const form = event.target as HTMLFormElement | null
+
+    if (!form) {
+      return
+    }
+
+    const formData = Object.fromEntries(new FormData(form)) as {qr_string: string}
+
+    const resultData: FormData = {...formData, timestamp: Date.now()}
+
+    console.log(resultData)
+
+    this.sendData(resultData)
+    .then(() => alert('Data uploaded successfuly.'))
+    .catch((reason: unknown) => {
+      console.error(reason)
+      alert('An error occurred after submiting the form.')
+    })
+  }
+
 }
-
-
-const API_ORIGIN = 'https://proyecto-inicial-backend-agk6kyxhfa-uc.a.run.app/'
 
 interface FormData {
   qr_string: string
   timestamp: number
 }
-
-async function sendData(data: FormData) {
-  const endpoint = new URL('/api/send-data/', API_ORIGIN)
-
-  const stringifiedData = JSON.stringify(data)
-
-  const response = await fetch(endpoint, {
-    method: 'POST',
-    body: stringifiedData,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-
-  return await response.json()
-}
-
-
-const form = document.querySelector('form')
-
-form?.addEventListener('submit', event => {
-  event.preventDefault()
-
-  const formData = Object.fromEntries(new FormData(form)) as {qr_string: string}
-
-  const resultData: FormData = {...formData, timestamp: Date.now()}
-
-  console.log(resultData)
-
-  sendData(resultData)
-  .then(value => alert('Data uploaded successfuly.'))
-  .catch(reason => {
-    console.error(reason)
-    alert('An error occurred after submiting the form.')
-  })
-})
