d08acdf9be310a0b75f2ea34f4c63a3e0b3fe883
Daniel Fabián <daniel@conectaentradas.com>
2024-03-21 16:34:01 +0100

    Modify server to fit better a SPA application

--- back_end/src/main.py
+++ back_end/src/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, FileResponse, Response
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import re
 
 app = FastAPI()
 
@@ -10,16 +11,10 @@ templates = Jinja2Templates(directory = templates_directory)
 
 
 entry_point = 'index.html'
-
-@app.get("/", response_class = HTMLResponse)
-def root(request: Request):
-  return templates.TemplateResponse(
-    request = request, name = entry_point, context = {}
-  )
-
-
-# Move the static files declaration at the bottom for it to work at the root ('/') level
-static_route = '/'
-static_directory = templates_directory
-
-app.mount(static_route, StaticFiles(directory = static_directory), name = "static")
+@app.get("/{full_path:path}", response_class = Response)
+def root(request: Request, full_path: str):
+  resource_path_regexp = r'^.+\..+$'
+  if re.match(string=full_path, pattern=resource_path_regexp) != None:
+    return FileResponse(f'./dist/browser/{full_path}')
+  
+  return FileResponse(f'./dist/browser/index.html')
