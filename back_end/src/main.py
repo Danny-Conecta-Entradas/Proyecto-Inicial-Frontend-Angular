ae7aa2a21248c93cd94cb4eaf8437655cd14340a
Daniel Fabián <daniel@conectaentradas.com>
2024-03-21 16:35:35 +0100

    Update main.py

--- back_end/src/main.py
+++ back_end/src/main.py
@@ -16,5 +16,5 @@ def root(request: Request, full_path: str):
   resource_path_regexp = r'^.+\..+$'
   if re.match(string=full_path, pattern=resource_path_regexp) != None:
     return FileResponse(f'./dist/browser/{full_path}')
-  
+
   return FileResponse(f'./dist/browser/index.html')
