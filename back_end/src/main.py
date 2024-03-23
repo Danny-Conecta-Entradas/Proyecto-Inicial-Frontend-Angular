017cae9eb279e9a3c1f9b4a079cde68004fcd7df
Daniel Fabián <daniel@conectaentradas.com>
2024-03-23 14:24:29 +0100

    Add handle for File Not Found and fix resource matching

--- back_end/src/main.py
+++ back_end/src/main.py
@@ -1,15 +1,23 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import FileResponse
-from fastapi.templating import Jinja2Templates
+from fastapi.responses import Response, FileResponse
 import re
+import os
 
 app = FastAPI()
 
-@app.get("/{full_path:path}", response_class = FileResponse)
+@app.get("/{full_path:path}", response_class = Response)
 def root(request: Request, full_path: str):
-  resource_path_regexp = r'^.+\..+$'
+  resource_path_regexp = r'^.*\..+$'
 
-  if re.match(string=full_path, pattern=resource_path_regexp) != None:
-    return FileResponse(f'./dist/browser/{full_path}')
+  # Split url parameters to only get the pathname
+  path = full_path.split('?')[0]
+
+  if re.match(string=path, pattern=resource_path_regexp) != None:
+    resource_path = f'./dist/browser/{path}'
+
+    if not os.path.isfile(resource_path):
+      return Response(status_code=404, content='Resource Not Found')
+
+    return FileResponse(resource_path)
 
   return FileResponse(f'./dist/browser/index.html')
